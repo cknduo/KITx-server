b0a8617e4001a720596b726dce63aa31c22c2945
mKhan-11 <80430158+mKhan-11@users.noreply.github.com>
2021-05-17 01:12:34 -0600

    aAdded login/register capability with encryption. Routes adjusted accordingly.

--- model/db.js
+++ model/db.js
@@ -2,18 +2,18 @@
 const mongoose = require ('mongoose')
 require ('dotenv').config()
 
-/* Moved to environment variable */
-//const dbUrl = 'mongodb+srv://project2_user:WiseWhales@evolveu.oaffh.mongodb.net/project2_database?retryWrites=true&w=majority'
+// Moved to environment variable
+// const dbUrl = 'mongodb+srv://project2_user:WiseWhales@evolveu.oaffh.mongodb.net/project2_database?retryWrites=true&w=majority'
 
 const databaseName = 'project2_database'
 
-/* 11-May-2021:  Updated mongodb connection to mongoose connection */
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+// 11-May-2021:  Updated mongodb connection to mongoose connection
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection;
 
 db.once('open', (_) =>
   console.log('MongoDB is now connected:', `${process.env.MONGODB_URL}`)
-);
+)
 
-db.on('error', (err) => console.error('MongoDB connection error!', err));
+db.on('error', (err) => console.error('MongoDB connection error!', err))
