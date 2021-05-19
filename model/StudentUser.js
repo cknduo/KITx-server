7daa3897e7d19fc624f8a8ab88741c88c3f0ed0e
mKhan-11 <80430158+mKhan-11@users.noreply.github.com>
2021-05-18 23:08:12 -0600

    database accounts linked by id, multiple fields schema added

---
+++ model/StudentUser.js
@@ -0,0 +1,18 @@
+require ('./db')
+const mongoose = require ('mongoose')
+const Schema = mongoose.Schema;
+
+const studentUserSchema = new Schema ({
+        firstName: String,
+        lastName: String,
+        address: String,
+        city: String,
+        province: String,
+        postalCode: String,
+        country: String,
+        userID: String,
+        coursesLearning: { enrolled: [String], bookmarked: [String], completed: [String] }
+},
+{ versionKey: false }) // Removes Mongoose __v attribute in database
+
+module.exports = mongoose.model ('StudentUser', studentUserSchema, 'studentUsers')
--- routes/login.js
+++ routes/login.js
@@ -11,7 +11,7 @@ router.post("/", (req, res, next) => {
         req.logIn(user, (err) => {
           if (err) throw err
           res.send("Successfully Authenticated")
-          console.log(req.user)
+          console.log("User authenticated from database is: ", req.user)
         });
       }
     })(req, res, next)
