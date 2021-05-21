e601352fa92e70d285ce02ef48a27dca33b0a949
PTorres <pstorres@gmail.com>
2021-05-21 03:01:03 -0600

    new files for backend

---
+++ routes/courseMaterialRecords.js
@@ -0,0 +1,81 @@
+const CourseMaterialRecords = require ('../model/CourseMaterialRecords')
+const express = require('express')
+const router = express.Router()
+
+/* get list of all courses*/
+router.get('/', async (req, res) => {
+    let data = await CourseMaterialRecords.find({})
+    console.info(`records retrieved from mongoose:`, data?.length)
+    res.send(data);
+})
+
+/* get by ID */
+router.get('/findByID/:id', async (req, res) => {
+  let data = await CourseMaterialRecords.findOne({_id: req.params.id})
+  try {
+      console.info(`course retrieved from mongoose:`, data)
+      res.send(data);
+  }
+  catch (error) {
+      console.error(error)
+      res.sendStatus(500)
+    }
+})
+
+/* get by courseID */
+router.get('/findByCourseID/:courseID', async (req, res) => {
+  let data = await CourseMaterialRecords.find({courseID: req.params.courseID})
+  try {
+      console.info(`course retrieved from mongoose:`, data)
+      res.send(data);
+  }
+  catch (error) {
+      console.error(error)
+      res.sendStatus(500)
+    }
+})
+
+
+/* get course name */
+router.get('/findByName/:courseName', async (req, res) => {
+  let data = await CourseMaterialRecords.findOne({courseName: req.params.courseName})
+  try {
+      console.info(`course retrieved from mongoose:`, data)
+      res.send(data);
+  }
+  catch (error) {
+      console.error(error)
+      res.sendStatus(500)
+    }
+})
+
+/* Update file info by fileID. */
+router.put('/:fileID', async function(req, res) {
+    let dataToUpdate = req.body
+    console.log("dataToUpdate",dataToUpdate)
+    try {
+      let data = await CourseMaterialRecords.findOneAndUpdate({fileID:req.params.fileID}, dataToUpdate, {new:true});
+      console.log("Updated course record", data)
+      res.send(data);
+    }
+    catch(error) {
+      console.log(error)
+      res.sendStatus(500)
+    }
+})
+
+router.post ('/addCourse', async (req, res) => {
+  let courseToCreate = req.body
+  try {
+    let newCourse = new CourseMaterialRecords(courseToCreate)
+    await newCourse.save()
+    console.log("Created course added to Course Material Record DB", newCourse)
+    res.send(newCourse)  
+  }
+  catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
+module.exports = router
