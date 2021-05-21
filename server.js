f08fe5650ace22fdc7b77e5ce65346aebea7a86f
PTorres <pstorres@gmail.com>
2021-05-21 02:17:12 -0600

    updated server routes, added schemas for db storage

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ require ('dotenv').config()
 const express = require('express')
 const app = express()
 
-const users = require ('./routes/users')
+//const users = require ('./routes/users')
+
+const teachers = require ('./routes/teacherUsers')
+const students = require ('./routes/studentUsers')
+const courseMaterial = require('./routes/courseMaterial')
+const courseMaterialRecords = require('./routes/courseMaterialRecords')
+
 const courses = require ('./routes/courses')
 const login = require ('./routes/login')
 const register = require ('./routes/register')
 const useridgetter = require ('./routes/useridgetter')
+
+
+const fileUpload = require("express-fileupload")
+
+
+
 
 const cors = require ('cors')
 const passport = require("passport")
@@ -16,6 +28,8 @@ const session = require("express-session")
 
 // Middleware
 app.use(express.json()) //JSON Parser
+app.use(fileUpload())
+
 app.use(
   cors({
     origin: "http://localhost:4444", // <-- location of the react app we're connecting to
@@ -35,7 +49,12 @@ app.use(passport.session());
 require("./passportConfig")(passport);
 
 // Routes
-app.use('/users', users)  //Fix and merge with users later {students, teachers}
+//app.use('/users', users)  //Fix and merge with users later {students, teachers}
+app.use('/students', students)
+app.use('/teachers', teachers)
+
+app.use('/courseMaterial', courseMaterial)
+app.use ('/courseMaterialRecords', courseMaterialRecords)
 app.use('/courses', courses)
 app.use('/login', login)
 app.use('/register', register)
