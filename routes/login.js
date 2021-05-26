36598e25f26ef23e97bba35f46d02c760f8b160d
mKhan-11 <80430158+mKhan-11@users.noreply.github.com>
2021-05-26 13:58:07 -0600

    useridgetter route changed to userinfo, data response changes upon login

--- routes/login.js
+++ routes/login.js
@@ -4,17 +4,16 @@ const passport = require("passport")
 
 router.post("/", (req, res, next) => {
   console.log("Login Request is: ", req.body)
-    passport.authenticate("local", (err, user, info) => {
-      if (err) throw err
-      if (!user) res.send("Invalid Username/Password or No User Exists")
-      else {
-        req.logIn(user, (err) => {
-          if (err) throw err
-          res.send("Successfully Authenticated")
-          console.log("User authenticated from database is: ", req.user)
-        });
-      }
-    })(req, res, next)
-  })
+  passport.authenticate("local", (err, user, info) => {
+    if (err) throw err
+    if (!user) res.send("Invalid Username/Password or No User Exists")
+    else {
+      req.logIn(user, (err) => {
+        if (err) throw err
+        res.send({msg: "Successfully Authenticated", user: user}) //Send user object as part of response
+      });
+    }
+  })(req, res, next)
+})
 
 module.exports = router
