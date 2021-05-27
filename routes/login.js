5c01c52a34105912d9f89762c032cc02dbe4dadd
mKhan-11 <80430158+mKhan-11@users.noreply.github.com>
2021-05-27 09:00:04 -0600

    Logout route added, login/userinfo route fixed

--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const passport = require("passport")
+const User = require('../model/User')
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
   console.log("Login Request is: ", req.body)
   passport.authenticate("local", (err, user, info) => {
     if (err) throw err
     if (!user) res.send("Invalid Username/Password or No User Exists")
     else {
-      req.logIn(user, (err) => {
+      req.logIn(user, async (err) => {
         if (err) throw err
-        res.send({msg: "Successfully Authenticated", user: user}) //Send user object as part of response
-      });
+        const info = await User.findOne({ "userID": user._id })
+        res.send({ "msg": "Successfully Authenticated", "userInfo": info, "userID": user._id })
+      })
     }
   })(req, res, next)
 })
--- routes/logout.js
+++ routes/logout.js
@@ -1,17 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const passport = require("passport")
 
-router.post("/", (req, res) => {
-    console.log("Login Request is: ", req.body)
-    passport.authenticate("local", (err, user, info) => {
-        if (err) throw err
-        if (!user) res.send("Invalid Username/Password or No User Exists")
-        else {
-            req.logOut()
-            res.send("You've been logged out")
-        }
-    })
+router.get("/", (req, res) => {
+    console.log("Logout Request is: ", req.body)
+    req.logout()
+    res.send("You've been logged out")
+    // res.redirect('/')
 })
 
 module.exports = router
